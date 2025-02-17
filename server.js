7893b97cc626cd65b3a08a243a2cfd929d3fb622
ManiSaliyan <138963170+ManiSaliyan@users.noreply.github.com>
2025-02-17 00:00:42 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+import './config/db.js';
+import express from 'express';
+import UserRoute from './api/User.js';
+import bodyParser from 'express';
+import cors from 'cors';
+const app = express();
+const port = process.env.PORT||3000;
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/user',UserRoute)
+
+app.listen(port,()=>{
+    console.log(`server running on PORT: ${port}`)
+})
